68814c83ed625e248f41d534390d3871e82e7cbe
jgnacio <jgnaciogomez406@gmail.com>
2023-03-22 09:34:18 -0700

    update place attributes

--- models/place.py
+++ models/place.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, String, ForeingKey
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
 
 
 class Place(BaseModel):
     """ A place to stay """
     __tablename__ = "places"
-    city_id = Column(String(60), nullable = False, ForeingKey("cities.id"))
-    user_id = Column(String(60), nullable = False, ForeingKey("users.id"))
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable = False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable = False)
     name = Column(String(128), nullable = False)
     description = Column((1024), nullable = False)
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    number_rooms = Column(Integer, nullable = False, default=0)
+    number_bathrooms = Column(Integer, nullable = False, default=0)
+    max_guest = Column(Integer, nullable = False, default=0)
+    price_by_night = Column(Integer, nullable = False, default=0)
+    latitude = Column(Float, nullable = False)
+    longitude = Column(Float, nullable = False)
     amenity_ids = []
