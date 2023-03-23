36223cd8106ed02599afe4ee1898bcbf1c3179b6
jgnacio <jgnaciogomez@gmail.com>
2023-03-23 11:10:55 -0300

    Add inheritance to Base, and restify ForeignKey

--- models/review.py
+++ models/review.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, String, ForeingKey
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
-    place_id = Column(String(60), nullable = False, ForeingKey("places.id"))
-    user_id = Column(String(60), nullable = False, ForeingKey("users.id"))
+    place_id = Column(String(60), ForeignKey("places.id"), nullable = False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable = False)
     text = Column(String(1024), nullable = False)
