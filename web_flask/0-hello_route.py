ee83d63822fef5ee8772c876b87ddec2927a6ae0
jgnacio <jgnaciogomez406@gmail.com>
2023-03-27 06:33:35 -0700

    pycodestyle: E302, E302, docstrings

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,15 @@
+#!/usr/bin/python3
+"""My first application with flask."""
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def say_hello():
     """Return hello HBNB on /."""
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
