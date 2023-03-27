729345aba19c0c821fc81fc76d4b88837a497f39
jgnacio <jgnaciogomez406@gmail.com>
2023-03-27 09:02:46 -0700

    remove changes

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,7 +4,7 @@ Created on Mon Mar 27 11:13:00 2023.
 
 @authors: jgnacio
 @description:
-    This module is an update of 1-hbnb_route.py that
+    This module is an update of 2-c_route.py that
     provides a simple web application with the flask
     framework, and have this endpoints available:
 
@@ -22,28 +22,42 @@ Created on Mon Mar 27 11:13:00 2023.
         (replace underscore _ symbols with a space ).
 
 """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def say_hello():
+    """Return hello HBNB on /."""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Return HBNB on /hbnb end point."""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    Display “C ” followed by the value of the text variable.
+    
+    (replace underscore _ symbols with a space ).
+    """
+    return f'C_{text}'.replace("_", " ")
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """
+    Display “Python ” followed by the value of the text variable.
+    
+    (replace underscore _ symbols with a space ).
+    """
+    return f'Python_{text}'.replace("_", " ")
+
+
 if __name__ == '__main__':
-    from flask import Flask
-
-    app = Flask(__name__)
-
-    @app.route('/', strict_slashes=False)
-    def say_hello():
-        """Return hello HBNB on /."""
-        return "Hello HBNB!"
-
-    @app.route('/hbnb', strict_slashes=False)
-    def hbnb():
-        """Return HBNB on /hbnb end point."""
-        return "HBNB"
-
-    @app.route('/c/<text>', strict_slashes=False)
-    def c(text):
-        """
-        Display “C ” followed by the value of the text variable.
-
-        (replace underscore _ symbols with a space ).
-        """
-        return f'C_{text}'.replace("_", " ")
-
     app.run(host='0.0.0.0', port=5000)
