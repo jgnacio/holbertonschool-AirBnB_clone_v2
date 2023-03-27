240b66b7df40ffb5833bbbb7424270ca80c0d1d5
jgnacio <jgnaciogomez406@gmail.com>
2023-03-27 09:00:40 -0700

    test not execute when import?

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -22,32 +22,28 @@ Created on Mon Mar 27 11:13:00 2023.
         (replace underscore _ symbols with a space ).
 
 """
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/', strict_slashes=False)
-def say_hello():
-    """Return hello HBNB on /."""
-    return "Hello HBNB!"
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """Return HBNB on /hbnb end point."""
-    return "HBNB"
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """
-    Display “C ” followed by the value of the text variable.
-    
-    (replace underscore _ symbols with a space ).
-    """
-    return f'C_{text}'.replace("_", " ")
-
-
 if __name__ == '__main__':
+    from flask import Flask
+
+    app = Flask(__name__)
+
+    @app.route('/', strict_slashes=False)
+    def say_hello():
+        """Return hello HBNB on /."""
+        return "Hello HBNB!"
+
+    @app.route('/hbnb', strict_slashes=False)
+    def hbnb():
+        """Return HBNB on /hbnb end point."""
+        return "HBNB"
+
+    @app.route('/c/<text>', strict_slashes=False)
+    def c(text):
+        """
+        Display “C ” followed by the value of the text variable.
+
+        (replace underscore _ symbols with a space ).
+        """
+        return f'C_{text}'.replace("_", " ")
+
     app.run(host='0.0.0.0', port=5000)
