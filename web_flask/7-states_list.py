49e2465ed291ab1e098ac7a809b677de3860f71a
jgnacio <jgnaciogomez406@gmail.com>
2023-03-29 08:46:33 -0700

    Fix pycodestyle and add documentation.

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,7 +4,12 @@ Created on Wed Mar 29 10:23:00 2023.
 
 @authors: jgnacio
 @description:
-    
+    This module provides a simple implementation of
+    the web framework flask, and have this endpoints.
+
+    (/states_list):
+        Display all states with the id and sorted by
+        the name.
 """
 from flask import Flask
 from flask import render_template
@@ -15,6 +20,7 @@ app = Flask(__name__)
 
 storage.reload()
 
+
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     """Shutdown the current session."""
@@ -23,8 +29,9 @@ def shutdown_session(exception=None):
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Display all states with the id and the name."""
-    new_dict = dict(sorted(storage.all(State).items(), key=lambda item: item[1].name))
+    """Display all states with the id and sorted by the name."""
+    new_dict = dict(sorted(storage.all(State).items(),
+                    key=lambda item: item[1].name))
     return render_template('7-states_list.html', states=new_dict.values())
 
 
